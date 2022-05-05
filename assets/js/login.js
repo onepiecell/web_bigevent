95ebf247d7960a59935fc801db8ad8b31b5ae52c
315249041@qq.com <315249041@qq.com>
2022-05-05 13:09:11 +0800

    完成了登录和注册

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,91 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+
+// 需用js重写
+
+// const { default: axios } = require("axios")
+
+// 登录/注册相互切换
+(function () {
+  // 点击去登陆账号的链接
+  document.querySelector('#link_reg').addEventListener('click', () => {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
+  // 点击去登录的链接
+  document.querySelector('#link_login').addEventListener('click', () => {
     $('.login-box').show()
     $('.reg-box').hide()
   })
-
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  // 表单验证
+  let form = layui.form
+  let layer = layui.layer
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
+    pass: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ]
+
+
+    , repwd: function (value) {
+      let pwd = document.querySelector('.reg-box #password').value
+      // let pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
         return '两次密码不一致！'
       }
     }
   })
+  // 注册提交事件
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
+  // 注册表单
+  document.querySelector('#form_reg').addEventListener('submit', function (e) {
     e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
+    let username = document.querySelector('#form_reg [name=username]').value
+    let password = document.querySelector('#form_reg [name=password]').value
+    let url = '/api/reguser'
+    let dataObj = { username: username, password: password }
+    // 有问题
+    // axios.post(url, dataObj).then(function (res) {
+    //   if (res.status !== 0) {
+    //     return console.log(res)
+    //   }
+    //   console.log('注册成功！');
+    // })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
+    $.post(
+      url,
+      dataObj,
+      function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message, {
+            time: 1000
+          })
+        }
+
+        layer.msg("注册成功,请登录", {
+          time: 1000
+        })
+        document.querySelector('#link_login').click()
+      }
+    )
+  })
+  // 登录表单
+  document.querySelector('#form_login').addEventListener('submit', function (e) {
     e.preventDefault()
     $.ajax({
+      method: 'post',
       url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function(res) {
+
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
         layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem('token', res.token)
-        // 跳转到后台主页
         location.href = '/index.html'
       }
     })
   })
-})
+
+
+})()
